024f77238f45c162ecfb1f9ae64be463e29f9b4b
Anthony Metzidis <anthony.metzidis@gmail.com>
2025-08-22 09:08:43 -0700

    input.codesign

--- scripts/windows/create-cert.ps1
+++ scripts/windows/create-cert.ps1
@@ -1,1 +1,1 @@
-$cert = New-SelfSignedCertificate -Subject "CN=Test Fabric Signing Certificate" -Type CodeSigningCert -CertStoreLocation Cert:\CurrentUser\My -HashAlgorithm SHA256
+New-SelfSignedCertificate -Subject "CN=Test Fabric Signing Certificate" -Type CodeSigningCert -CertStoreLocation Cert:\CurrentUser\My -HashAlgorithm SHA256
--- scripts/windows/export-cert.ps1
+++ scripts/windows/export-cert.ps1
@@ -1,7 +1,7 @@
 # Export signing certificate into base64 encoded contents
 $cert= Get-ChildItem -Path Cert:\CurrentUser\My | where-object  {$_.Subject -like "*fabric*"}
-if ($cert -eq $null) {
-    Write-Error "Signing certificate 'fabric-ca-cert' not found in LocalMachine\My store."
+if ($null -eq $cert) {
+    Write-Error "Signing certificate '*fabric*' not found in LocalMachine\My store."
     exit 1
 }
 
